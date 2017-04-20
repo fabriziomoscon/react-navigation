5028258480c8ea4eb4754ca9ae5b61538f61c934
Kevin Cooper <cooperka92@gmail.com>
2017-04-20 12:07:27 -0400

    It even works with nested StackNav

--- examples/ReduxExample/src/navigators/AppNavigator.js
+++ examples/ReduxExample/src/navigators/AppNavigator.js
@@ -1,13 +1,13 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { addNavigationHelpers, DrawerNavigator } from 'react-navigation';
+import { addNavigationHelpers, DrawerNavigator, StackNavigator } from 'react-navigation';
 
 import LoginScreen from '../components/LoginScreen';
 import MainScreen from '../components/MainScreen';
 import ProfileScreen from '../components/ProfileScreen';
 
 export const AppNavigator = DrawerNavigator({
-  Login: { screen: LoginScreen },
+  LoginStack: { screen: StackNavigator({ Login: { screen: LoginScreen } }) },
   Main: { screen: MainScreen },
   Profile: { screen: ProfileScreen },
 });
