d8e2d72eaf4c7d158d76acb303f31ff81e1a40c1
Kevin Cooper <cooperka92@gmail.com>
2017-05-02 15:31:51 -0400

    Add CustomDrawerItems from NavigationPlayground

--- examples/ReduxExample/src/navigators/AppNavigator.js
+++ examples/ReduxExample/src/navigators/AppNavigator.js
@@ -1,16 +1,75 @@
 import React, { PropTypes } from 'react';
+import { View, StyleSheet, Platform } from 'react-native';
 import { connect } from 'react-redux';
-import { addNavigationHelpers, DrawerNavigator } from 'react-navigation';
+import { addNavigationHelpers, DrawerNavigator, DrawerItems, DrawerItem } from 'react-navigation';
 
 import LoginScreen from '../components/LoginScreen';
 import MainScreen from '../components/MainScreen';
 import ProfileScreen from '../components/ProfileScreen';
+
+const styles = StyleSheet.create({
+  drawerContainer: {
+    marginTop: Platform.OS === 'ios' ? 20 : 0,
+    paddingVertical: 4,
+  },
+  drawerContainerInner: {
+    marginTop: 0,
+    paddingVertical: 0,
+  },
+});
+
+const CustomDrawerItem = (props) => {
+  const { item, focused, activeTintColor, inactiveTintColor } = props;
+  const tintColor = focused ? activeTintColor : inactiveTintColor;
+
+  const screenOptions = typeof item.screenOptions === 'function'
+    ? item.screenOptions(props)
+    : item.screenOptions;
+  const { drawerIcon, drawerLabel, drawerOnPress } = screenOptions;
+
+  return (
+    <DrawerItem
+      {...props}
+      focused={false}
+      icon={drawerIcon ? drawerIcon({ tintColor }) : null}
+      label={drawerLabel}
+      onPress={drawerOnPress}
+    />
+  );
+};
+
+const CustomDrawerItems = (props) => {
+  const { items } = props;
+
+  return (
+    <View style={[styles.drawerContainer]}>
+      {items.map((item) => {
+        const { routeName, key } = item;
+
+        // Simply render the default component if the item is a route.
+        if (routeName) {
+          return (
+            <DrawerItems
+              {...props}
+              key={key}
+              items={[item]}
+              style={styles.drawerContainerInner}
+            />
+          );
+        }
+
+        return <CustomDrawerItem {...props} key={key} item={item} />;
+      })}
+    </View>
+  );
+};
 
 export const AppNavigator = DrawerNavigator({
   Login: { screen: LoginScreen },
   Main: { screen: MainScreen },
   Profile: { screen: ProfileScreen },
 }, {
+  contentComponent: CustomDrawerItems,
   contentOptions: {
     items: [
       { key: 'Login', routeName: 'Login' },
